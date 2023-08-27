116d11162ed59905ca7092ad855160ad9cbb0e76
sebas3536 <delahozpablo005@gmail.com>
2023-08-26 23:58:05 -0500

    se realizan cambios

--- js/script.js
+++ js/script.js
@@ -112,3 +112,38 @@ document.addEventListener("pointerdown", (e) => {
 });
 /******************************************* */
 const toggleMenuOpen = () => document.body.classList.toggle("open");
+/******************************************************/ 
+const toggleMenu = () => {
+  const navigation = document.querySelector('.navigation');
+
+
+  const burgerMenu = document.querySelector(".menu-icon");
+  const src = burgerMenu.getAttribute('src');
+
+  const isBurger = src === 'img/burger-menu.svg';
+
+  const iconName = isBurger ?
+      'img/close.svg'
+      :
+      'img/burger-menu.svg"';
+
+
+  burgerMenu.setAttribute(
+      'src',
+      iconName
+  );
+
+  if (!isBurger) {
+      navigation.classList.add("navigation--mobile--fadeout");
+      setTimeout(() => {
+          navigation.classList.toggle(
+              'navigation--mobile'
+          );
+      }, 300)
+  } else {
+      navigation.classList.remove("navigation--mobile--fadeout");
+      navigation.classList.toggle(
+          'navigation--mobile'
+      );
+  }
+};
